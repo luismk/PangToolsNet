516a40e5a967d591d7d26629c5844ce19d75dc77
LuisMK <luizinrc@hotmail.com>
2021-11-18 21:27:54 -0300

    Update Program.cs

--- TestTools/Program.cs
+++ TestTools/Program.cs
@@ -1,26 +1,66 @@
-﻿using System;
+using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
+
 using UpdateList;
 
+
+
 namespace TestTools
+
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
-            string filePath = @"C:\\Users\\eric.silva\\desktop\\updatelist";
-            string fileOutput = filePath + ".xml" ;
 
-            var decrypted = new FileCrypt().DecryptEncryptFile(filePath, FileCrypt.KeyEnum.JP, FileCrypt.OperacaoEnum.Decrypt);
-            System.IO.File.WriteAllBytes(fileOutput, decrypted);
 
-            var encrypted = new FileCrypt().DecryptEncryptFile(fileOutput, FileCrypt.KeyEnum.JP, FileCrypt.OperacaoEnum.Encrypt);
-            System.IO.File.WriteAllBytes(filePath + "_encrypt", encrypted);
 
+            Console.WriteLine("UpdateList - Encrypt/Decrypt ");
+
+            Console.WriteLine("Wait insert to file...");
+
+            for (; ; )
+
+            {
+
+                if (args.Length > 0)
+
+                {
+
+                    string filePath = args[0];
+
+                    for (int i = 0; i < 6; i++)
+
+                    {
+
+                        var result = new FileCrypt().DecryptEncryptFile(filePath, out byte[] decrypted, (FileCrypt.KeyEnum)i);
+
+         
+
+                        if (FileCrypt.Result.Sucess == result)
+
+                            break;
+
+                    }
+
+                }
+
+                Console.ReadLine();
+
+            }
 
         }
+
     }
 }
